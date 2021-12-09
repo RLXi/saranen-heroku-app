ae6414282837b31fff013bfe83db7301d9851b74
Aleksi Hynönen <rolexpost@gmail.com>
2021-12-09 15:04:35 +0200

    UI

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,44 @@
 const ul = document.querySelector(".people");
+const getBtn = document.getElementById("get");
+const postBtn = document.getElementById("post");
+const deleteBtn = document.getElementById("delete");
+const locationList = document.querySelector(".location-list");
+const url = "/locations";
+
+getBtn.addEventListener("click", async (e) => {
+  const res = await fetch(url);
+  const json = await res.json();
+  locationList.innerHTML = json
+    .map(
+      (loc) =>
+        `<li>id: ${loc.id} lat: ${loc.latitude} long: ${loc.longitude}</li>`
+    )
+    .join("");
+});
+
+postBtn.addEventListener("click", async (e) => {
+  const lat = document.querySelector('[name="lat"]');
+  const long = document.querySelector('[name="long"]');
+  const obj = {
+    longitude: parseInt(long.value),
+    latitude: parseInt(lat.value),
+  };
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(obj),
+  });
+  const json = await res.json();
+});
+
+deleteBtn.addEventListener("click", async (e) => {
+  const del = document.querySelector('[name="del"]');
+  const res = await fetch(`${url}/${del.value}`, { method: "DELETE" });
+  const json = await res.json();
+  alert(json.msg);
+});
 
 async function fetchPeople() {
   const res = await fetch("/people");
--- routes/location.js
+++ routes/location.js
@@ -1,16 +1,62 @@
 const express = require("express");
+const cors = require("cors");
 const { locationsDB } = require("../db");
 const locations = express.Router();
 
+let locBase = [...locationsDB];
+
 locations.use(express.json());
+locations.use(cors());
 
 locations.get("/", (req, res) => {
-  res.status(200).send(locationsDB);
+  res.status(200).send(locBase);
 });
 
 locations.get("/:id", (req, res) => {
-  const loc = locationsDB.find((l) => l.id === parseInt(req.params.id));
+  const loc = locBase.find((l) => l.id === parseInt(req.params.id));
+  if (!loc) {
+    res.status(400).json({ msg: "No such ID" });
+    return;
+  }
   res.status(200).json(loc);
+});
+
+locations.post("/", (req, res) => {
+  const id = locBase.reduce(
+    (acc, val) => (val.id >= acc ? val.id + 1 : acc),
+    0
+  );
+
+  const loc = req.body;
+  if (!loc.hasOwnProperty("longitude") || !loc.hasOwnProperty("latitude")) {
+    res.status(400).send({ msg: "Failed to post, invalid properties" });
+    return;
+  }
+  if (isNaN(loc.longitude) || isNaN(loc.latitude)) {
+    res.status(400).send({ msg: "Failed to post, invalid property values" });
+    return;
+  }
+  loc.id = id;
+  locBase.push(loc);
+
+  res.status(201).send(loc);
+});
+
+locations.patch("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  res.status(202).send({ msg: "Entry updated" });
+});
+
+locations.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const newBase = locBase.filter((loc) => loc.id !== id);
+  if (newBase.length === locBase.length) {
+    res.status(404).send({ msg: "Failed to delete" });
+    return;
+  }
+  locBase = newBase;
+
+  res.status(200).send({ msg: "Entry deleted" });
 });
 
 locations.get("/about", (req, res) => {
